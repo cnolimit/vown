92354b9f7abd6d0e2d0a360e2cffdd0c45dc1924
Phillip Boateng <cnolimit@gmail.com>
2019-08-02 12:59:15 +0100

    reset .gitignore & added prettier file

--- packages/vown-reviews/functions/src/index.ts
+++ packages/vown-reviews/functions/src/index.ts
@@ -1,47 +1,47 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import * as express from "express";
-import * as cors from "cors";
-import * as bodyParser from "body-parser";
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+import * as express from 'express'
+import * as cors from 'cors'
+import * as bodyParser from 'body-parser'
 
-import { IReview } from "./types";
-const app = express();
-admin.initializeApp();
+import { IReview } from './types'
+const app = express()
+admin.initializeApp()
 
-app.use(cors({ origin: true }));
-app.use(bodyParser.json());
+app.use(cors({ origin: true }))
+app.use(bodyParser.json())
 
-app.get("/", (req, res) => res.send("Welcome to the reviews API"));
+app.get('/', (req, res) => res.send('Welcome to the reviews API'))
 
-app.post("/reviews", async (req, res) => {
-  const review: IReview = req.body;
+app.post('/reviews', async (req, res) => {
+  const review: IReview = req.body
 
   try {
     await admin
       .firestore()
-      .collection("reviews")
+      .collection('reviews')
       .doc()
-      .create({ ...review });
+      .create({ ...review })
 
-    res.send("Created Review!");
+    res.send('Created Review!')
   } catch (e) {
-    res.status(422).send(`Failed to create review - Error: ${e.message}`);
+    res.status(422).send(`Failed to create review - Error: ${e.message}`)
   }
-});
+})
 
-app.get("/reviews", async (req, res) => {
+app.get('/reviews', async (req, res) => {
   try {
     const reviews = await admin
       .firestore()
-      .collection("reviews")
+      .collection('reviews')
       .doc()
-      .get();
-    console.log(reviews);
+      .get()
+    console.log(reviews)
 
-    res.send({ data: reviews.data() });
+    res.send({ data: reviews.data() })
   } catch (e) {
-    res.status(422).send(`Failed to create review - Error: ${e.message}`);
+    res.status(422).send(`Failed to create review - Error: ${e.message}`)
   }
-});
+})
 
-exports.api = functions.https.onRequest(app);
+exports.api = functions.https.onRequest(app)

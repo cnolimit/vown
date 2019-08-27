9d9ad2e0ec5ff258812e0d06899b4cadf3188771
Phillip Boateng <cnolimit@gmail.com>
2019-08-27 20:54:46 +0100

    added a new function to create user in firestore

--- packages/vown-functions/functions/index.ts
+++ packages/vown-functions/functions/index.ts
@@ -3,6 +3,7 @@ import * as admin from 'firebase-admin'
 import * as express from 'express'
 import * as cors from 'cors'
 import * as bodyParser from 'body-parser'
+import * as uuid from 'uuid/v4'
 import apiReviewsV1 from './api/v1/reviews'
 import apiSessionsV1 from './api/v1/sessions'
 
@@ -18,3 +19,22 @@ app.use('/sessions', apiSessionsV1)
 app.use('/reviews', apiReviewsV1)
 
 exports.api = functions.https.onRequest(app)
+
+exports.createUser = functions.auth.user().onCreate(user => {
+  const { email, uid, metadata, displayName, photoURL, phoneNumber } = user
+  console.log({ user })
+
+  return admin
+    .firestore()
+    .collection('users')
+    .doc(uid)
+    .set({
+      uid,
+      email,
+      phoneNumber,
+      photoURL,
+      displayName,
+      token_id: uuid(),
+      createdAt: metadata.creationTime,
+    })
+})

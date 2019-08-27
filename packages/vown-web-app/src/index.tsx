aaf50fd48263128f82b0cf3bb49a9d04738ba6e9
Phillip Boateng <cnolimit@gmail.com>
2019-08-27 20:57:39 +0100

    testing the auth package

--- packages/vown-web-app/src/index.tsx
+++ packages/vown-web-app/src/index.tsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import * as ReactDOM from 'react-dom'
-import ReviewContainer from '@components/reviews/reviews.container'
+// import ReviewContainer from '@components/reviews/reviews.container'
+import auth from '@vown/auth'
+
+const signIn = async () => {
+  const session = await auth.SignIn('cnolimit@gmail.com', 'pass123').catch(err => {
+    console.log({ err })
+  })
+  console.log({ session })
+  return session
+}
+
+const signUp = async () => {
+  const session = await auth.SignUp('cnolimit@gmail.com', 'pass123')
+  console.log({ session })
+  return session
+}
+
+const getUserDetails = async () => {
+  const user = await auth.GetPersonalDetails()
+  console.log({ user })
+  return user
+}
+
+const getToken = async () => {
+  const token = await auth.GetToken()
+  console.log({ token })
+  return token
+}
+
+const signOut = async () => await auth.SignOut()
 
 const App = () => {
-  return <ReviewContainer />
+  return (
+    <React.Fragment>
+      <div>
+        <button onClick={signIn}>Sign In</button>
+        <button onClick={signUp}>Sign Up</button>
+        <button onClick={signOut}>Sign Out</button>
+        <button onClick={getUserDetails}>Get User Details</button>
+        <button onClick={getToken}>Get User TOKEN</button>
+      </div>
+      {/* <ReviewContainer /> */}
+    </React.Fragment>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))

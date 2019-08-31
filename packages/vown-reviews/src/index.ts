42f6ba79eeaf4f760be702b91d100828e6fac620
Phillip Boateng <cnolimit@gmail.com>
2019-08-31 08:07:25 +0100

    moved api values into the class to share more values

--- packages/vown-reviews/src/index.ts
+++ packages/vown-reviews/src/index.ts
@@ -1,38 +1,40 @@
-import axios, { AxiosResponse } from 'axios'
+import axios, { AxiosResponse, AxiosInstance } from 'axios'
 import { IReview, IReviewUpdate } from '@vown/types'
 
 type Callback = (error: string | null, data?: object) => void
 
-const version = 'v1'
-const host = 'https://us-central1-veriown-reviews.cloudfunctions.net/api/reviews'
-
-const reviewsApi = axios.create({
-  baseURL: `${host}/${version}`,
-  timeout: 25000,
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-  },
-})
-
 class Review {
   private token: string
   private user: string
+  private reviewsApi: AxiosInstance
+  private version = 'v1'
+  private host = 'https://us-central1-veriown-reviews.cloudfunctions.net/api/reviews'
 
   constructor(token: string, user: string) {
     this.token = token
     this.user = user
+
+    this.reviewsApi = axios.create({
+      baseURL: `${this.host}/${this.version}`,
+      timeout: 25000,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        token: this.token,
+        user: this.user,
+      },
+    })
   }
 
   create(review: IReview, callback: Callback) {
-    reviewsApi
+    this.reviewsApi
       .post('/create', review)
       .then(res => callback(null, res.data))
       .catch((res: AxiosResponse) => callback(res.data))
   }
 
   update(review: IReviewUpdate, callback: Callback) {
-    reviewsApi
+    this.reviewsApi
       .put('/update', review)
       .then(res => callback(null, res.data))
       .catch((res: AxiosResponse) => callback(res.data))
@@ -41,19 +43,14 @@ class Review {
   retrieve() {
     return {
       landlord: (id: string, callback: Callback) => {
-        reviewsApi
+        this.reviewsApi
           .get(`/landlord/${id}`)
           .then(res => callback(null, res.data))
           .catch((res: AxiosResponse) => callback(res.data))
       },
       user: (callback: Callback) => {
-        reviewsApi
-          .get(`/user`, {
-            headers: {
-              token: this.token,
-              user: this.user,
-            },
-          })
+        this.reviewsApi
+          .get(`/user`)
           .then(res => callback(null, res.data))
           .catch((res: AxiosResponse) => callback(res.data))
       },

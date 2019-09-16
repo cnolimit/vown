20a7a8a18bfe1f6d941a0a3bddd9040f35f7d061
Phillip Boateng <cnolimit@gmail.com>
2019-09-16 13:04:09 +0100

    created tests for review package

--- packages/vown-reviews/src/index.ts
+++ packages/vown-reviews/src/index.ts
@@ -1,58 +1,49 @@
-import axios, { AxiosResponse, AxiosInstance } from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { IReview, IReviewUpdate } from '@vown/types'
 
-type Callback = (error: string | null, data?: object) => void
-
 class Review {
-  private token: string
-  private user: string
-  private reviewsApi: AxiosInstance
+  private baseURL: any
+  private headers: any
   private version = 'v1'
   private host = `https://us-central1-veriown-reviews.cloudfunctions.net/api/${this.version}reviews`
 
   constructor(token: string, user: string) {
-    this.token = token
-    this.user = user
-
-    this.reviewsApi = axios.create({
-      baseURL: `${this.host}/${this.version}`,
-      timeout: 25000,
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        token: this.token,
-        user: this.user,
-      },
-    })
+    this.baseURL = `${this.host}/${this.version}`
+    this.headers = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      token: token,
+      user: user,
+    }
   }
 
-  create(review: IReview, callback: Callback) {
-    this.reviewsApi
-      .post('/create', review)
-      .then(res => callback(null, res.data))
-      .catch((res: AxiosResponse) => callback(res.data))
+  create(review: IReview) {
+    return axios
+      .post(`${this.baseURL}/create`, review, { headers: this.headers })
+      .then(res => res.data)
+      .catch((res: AxiosResponse) => res.data)
   }
 
-  update(review: IReviewUpdate, callback: Callback) {
-    this.reviewsApi
-      .put('/update', review)
-      .then(res => callback(null, res.data))
-      .catch((res: AxiosResponse) => callback(res.data))
+  update(review: IReviewUpdate) {
+    return axios
+      .put(`${this.baseURL}/update`, review, { headers: this.headers })
+      .then(res => res.data)
+      .catch((res: AxiosResponse) => res.data)
   }
 
   retrieve() {
     return {
-      landlord: (id: string, callback: Callback) => {
-        this.reviewsApi
-          .get(`/landlord/${id}`)
-          .then(res => callback(null, res.data))
-          .catch((res: AxiosResponse) => callback(res.data))
+      landlord: (id: string) => {
+        return axios
+          .get(`${this.baseURL}/landlord/${id}`, { headers: this.headers })
+          .then(res => res.data)
+          .catch((res: AxiosResponse) => res.data)
       },
-      user: (callback: Callback) => {
-        this.reviewsApi
-          .get(`/user`)
-          .then(res => callback(null, res.data))
-          .catch((res: AxiosResponse) => callback(res.data))
+      user: () => {
+        return axios
+          .get(`${this.baseURL}/user`, { headers: this.headers })
+          .then(res => res.data)
+          .catch((res: AxiosResponse) => res.data)
       },
     }
   }

0db99cd04ad496bf72dc179a27e1ed1112e715d9
Phillip Boateng <cnolimit@gmail.com>
2019-09-18 01:05:44 +0100

    fixed build issue with firebase types

--- packages/vown-auth/src/index.ts
+++ packages/vown-auth/src/index.ts
@@ -1,7 +1,11 @@
-import * as firebase from 'firebase'
+/// <reference lib="dom" />
+import * as firebase from 'firebase/app'
 import { IUserDetails } from '@vown/types'
 import localforage from 'localforage'
 import axios from 'axios'
+import 'firebase/auth'
+import 'firebase/firestore'
+
 class Auth {
   private app = firebase.initializeApp({
     apiKey: 'AIzaSyARkIZDrZNFCSYd6At-tOmy8pLBY3EN3ng',
@@ -12,31 +16,25 @@ class Auth {
     messagingSenderId: '769049122151',
     appId: '1:769049122151:web:46fcf538595daaa2',
   })
-
   private AUTH_STORAGE = '__vown-session__'
   private AUTH_TOKEN = '__vown-session-token__'
   private _setSession = async (data: IUserDetails | null) =>
     localforage.setItem(this.AUTH_STORAGE, data)
   private _setToken = async (token: string) => localforage.setItem(this.AUTH_TOKEN, token)
-
   private _deleteSession = async () => localforage.removeItem(this.AUTH_STORAGE)
   private _deleteToken = async () => localforage.removeItem(this.AUTH_TOKEN)
-
   private _getPersonalDetails = async (): Promise<IUserDetails | null> => {
     const user = this.app.auth().currentUser
-
     if (user) {
       const userDetails = await firebase
         .firestore()
         .collection('users')
         .doc(user.uid)
         .get()
-
       return userDetails.data() as IUserDetails
     }
     return null
   }
-
   private _getToken = async (token_id: string, user: string) => {
     const res = await axios.post(
       'https://us-central1-veriown-reviews.cloudfunctions.net/v1/api/sessions/token',
@@ -44,7 +42,6 @@ class Auth {
     )
     return res.data
   }
-
   public SignIn = async (username: string, password: string) => {
     await this.app.auth().signInWithEmailAndPassword(username, password)
     const details = await this._getPersonalDetails()
@@ -55,7 +52,6 @@ class Auth {
     }
     return await this._getPersonalDetails()
   }
-
   public SignUp = async (username: string, password: string) => {
     await this.app.auth().createUserWithEmailAndPassword(username, password)
     const details = await this._getPersonalDetails()
@@ -66,24 +62,19 @@ class Auth {
     }
     return await this._getPersonalDetails()
   }
-
   public SignOut = async () => {
     await this._deleteSession()
     await this._deleteToken()
     this.app.auth().signOut()
   }
-
   public isLoggedIn = async () => !!this.app.auth().currentUser
-
   public GetId = async () => {
     const user = (await localforage.getItem(this.AUTH_STORAGE)) as IUserDetails
     if (user) return user.uid
     return null
   }
-
   public GetPersonalDetails = async (): Promise<IUserDetails | null> =>
     await localforage.getItem(this.AUTH_STORAGE)
-
   public GetToken = async (): Promise<string | null> => await localforage.getItem(this.AUTH_TOKEN)
 }
 

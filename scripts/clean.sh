889596865d7c5a2e6ecf0d0a9e621c595d8f5a5b
Phillip Boateng <cnolimit@gmail.com>
2019-09-20 13:43:57 +0100

    added components to the clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,5 +5,6 @@ rm -rf packages/vown-auth/node_modules
 rm -rf packages/vown-functions/functions/node_modules
 rm -rf packages/vown-web-app/node_modules
 rm -rf packages/vown-reviews/node_modules
+rm -rf packages/vown-components/node_modules
 
 echo "✅ - Removed node_modules"

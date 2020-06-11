604c00c89356718410b911f417715a7d2e8e6a9b
Phillip Boateng <cnolimit@gmail.com>
2020-06-11 13:30:46 +0100

    removed unused code from dangerfile

--- internals/danger/constants.js
+++ internals/danger/constants.js
@@ -1,11 +1,12 @@
 /**
  * @description An array of all the files we want to ignore
+ * from the included list
  */
-const IGNORE_LIST = ['__tests__', '/.', 'tsconfig.json'];
+const IGNORE_LIST = ['__tests__', '/.', 'tsconfig.json']
 
 /**
  * @description An array of all the files we want to include
  */
-const INCLUDE_LIST = ['packages'];
+const INCLUDE_LIST = ['packages']
 
-module.exports = { IGNORE_LIST, INCLUDE_LIST };
+module.exports = { IGNORE_LIST, INCLUDE_LIST }
